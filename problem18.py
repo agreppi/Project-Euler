92387bdeed07978a0f3b14ddb00cf37b6e51e2b4
agrepp001c <agrepp001c@cable.comcast.com>
2016-08-01 17:18:23 -0300

    Creating new problem18 module

--- problem18.py
+++ problem18.py
@@ -11,17 +11,29 @@ def main():
     f.close()
     fila = 0
     columna = 0
-    suma = triangle[fila][columna]
-    # pdb.set_trace()
-    for fila in range(1,len(triangle)):
-        if triangle[fila][columna] > triangle[fila][columna+1]:
-            sumando = triangle[fila][columna]
+    ultima_fila = triangle[len(triangle)-1]
+    largo_ultima_fila = len(ultima_fila)
+    max = 0
+    for i in range(largo_ultima_fila):
+        if ultima_fila[i] >= max:
+            max = ultima_fila[i]
+            indice_del_mayor = i
+    suma = max
+
+    for fila in range(len(triangle)-1, -1, -1):
+        # pdb.set_trace()
+        print(fila, indice_del_mayor)
+        if indice_del_mayor == fila + 1:
+            sumando = triangle[fila][indice_del_mayor - 1]
+            indice_del_mayor = indice_del_mayor - 1
+        elif triangle[fila][indice_del_mayor] > triangle[fila][indice_del_mayor-1]:
+            sumando = triangle[fila][indice_del_mayor]
         else:
-            sumando = triangle[fila][columna+1]
-            columna += 1
+            sumando = triangle[fila][indice_del_mayor-1]
+            indice_del_mayor = indice_del_mayor - 1
         print(suma, sumando)
         suma = suma + sumando
-
+    print(fila)
     elapsed = (time.time() - start)
     print("%s found in %s seconds" % (suma,elapsed))
 
